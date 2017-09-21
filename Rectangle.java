a915014652084689df9b1fb2f742422e5b768d8b
Justin Kaulback <jkaulbac@unb.ca>
2017-09-21 20:56:28 -0300

    Initial commit

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double l,double w) {
+
+        length = l;
+        width = w;
+
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+
+        return 2*(length + width);
+    }
+
+}
---
+++ RectangleTest.java
@@ -0,0 +1,15 @@
+public class RectangleTest {
+
+    public static void main(String[] args) {
+
+        Rectangle bedroom = new Rectangle(8.5,11.5);
+
+        System.out.println("The area of this rectangle is: " + bedroom.calculateArea() + "\n");
+
+        System.out.println("The perimeter of this rectangle is: " + bedroom.calculatePerimeter() + "\n");
+    }
+
+
+
+
+}
