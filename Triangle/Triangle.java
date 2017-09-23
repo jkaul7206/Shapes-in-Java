53c32a674c24b525e6eddbd82180cdfe00f78a4f
jkaul7206 <32168397+jkaul7206@users.noreply.github.com>
2017-09-23 14:02:28 -0300

    Create Triangle.java

---
+++ Triangle/Triangle.java
@@ -0,0 +1,18 @@
+/**
+This class will be used to create a right angled triangle
+which can be used for several applications.
+@author Justin Kaulback
+*/
+
+public class RightTriangle {
+    
+    private double adj;
+    private double opp;
+    
+        public RightTriangle(double a, double b) {
+        
+            
+        
+        }
+
+}
